c6ced365081afd37f46fd055d8b7a0d612e21f2b
Betnix CEO <gstar3181@gmail.com>
2026-02-25 17:59:32 -0500

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://pythoncoders45.github.io/micobot/](https://pythoncoders45.github.io/micobot/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/pythoncoders45/micobot** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/pythoncoders45/micobot** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,91 @@
+function right () {
+    motionbit.setServoPosition(MotionBitServoChannel.S1, 90)
+}
+input.onPinPressed(TouchPin.P0, function () {
+    forward()
+})
+function left () {
+    motionbit.setServoPosition(MotionBitServoChannel.S1, -90)
+}
+function connecttowifi () {
+    WiFiBit.connectToWiFiNetwork("SSID", "key")
+    WiFiBit.connectToWiFiBit()
+}
+input.onButtonPressed(Button.A, function () {
+    left()
+})
+function getenviromentdata () {
+    enviromentdata = [
+    Environment.sonarbit_distance(Environment.Distance_Unit.Distance_Unit_mm, DigitalPin.P2),
+    Environment.UVLevel(AnalogPin.P2),
+    Environment.ReadNoise(AnalogPin.P2),
+    Environment.ReadLightIntensity(AnalogPin.P2),
+    Environment.ReadWindSpeed(AnalogPin.P2)
+    ]
+}
+function backward () {
+    motionbit.runMotor(MotionBitMotorChannel.M2, MotionBitMotorDirection.Backward, 128)
+}
+function getsoildata () {
+    soildata = [
+    Environment.ReadSoilHumidity(AnalogPin.P2),
+    Environment.Ds18b20Temp(DigitalPin.P2, Environment.ValType.DS18B20_temperature_C),
+    Environment.ReadDust(DigitalPin.P2, AnalogPin.P2),
+    Environment.ReadSoilHumidity(AnalogPin.P2),
+    Environment.ReadWaterLevel(AnalogPin.P2),
+    Environment.ReadLightIntensity(AnalogPin.P2)
+    ]
+}
+input.onButtonPressed(Button.AB, function () {
+    getsoildata()
+    getwaterdata()
+    getenviromentdata()
+})
+input.onButtonPressed(Button.B, function () {
+    right()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    backward()
+})
+function overheating () {
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+function forward () {
+    motionbit.runMotor(MotionBitMotorChannel.M1, MotionBitMotorDirection.Forward, 128)
+}
+function disconnectfromwifi () {
+    WiFiBit.connectToWiFiBit()
+    WiFiBit.disconnectFromWiFiNetwork()
+}
+function getwaterdata () {
+    waterdata = [Environment.readPHLevel(AnalogPin.P2), Environment.ReadWaterLevel(AnalogPin.P2), Environment.ReadLightIntensity(AnalogPin.P2)]
+}
+let sciencedata = ""
+let temp = 0
+let waterdata: number[] = []
+let soildata: number[] = []
+let enviromentdata: number[] = []
+basic.showString("Hello! <:")
+basic.forever(function () {
+    temp = input.temperature()
+    sciencedata = "" + enviromentdata + soildata + waterdata
+    if (temp >= 35) {
+        while (temp >= 35) {
+            music.play(music.stringPlayable("C5 - C5 - C5 - C5 - ", 120), music.PlaybackMode.UntilDone)
+            overheating()
+        }
+    }
+})
